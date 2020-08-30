18e964f26ec9cac71074ff10f0fdd2607356c476
marlonchiu <Knight1898@outlook.com>
2020-08-30 17:00:08 +0800

    feat:Webpack构建环境(版本差异，一定要对照官方文档来)

---
+++ imooc-es-webpack/build/webpack.base.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'entry'
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'static', to: 'static' }]
+    })
+  ]
+}
---
+++ imooc-es-webpack/src/index.js
@@ -0,0 +1,3 @@
+const sum = (x, y) => x + y
+console.log(sum(5, 6))
+console.log(sum(7, 8))
